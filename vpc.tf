67e7e65454d85f85845151f4b1f85fdd70822e50
pragmauribe <ivan.uribe@pragma.com.co>
2023-01-04 20:05:38 -0500

    Adding VPC

---
+++ vpc.tf
@@ -0,0 +1,24 @@
+# Resource: aws_vpc
+# Provides a VPC resource
+resource "aws_vpc" "VPC_Pragma" {
+  cidr_block           = var.CIDR_VPC[0]
+  enable_dns_hostnames = true
+
+  tags = merge(
+    var.info_tags, {
+      Name = "VPC_Pragma"
+    }
+  )
+}
+
+# Resource: aws_internet_gateway
+# Provides a resource to create a VPC Internet Gateway
+resource "aws_internet_gateway" "gw_Pragma" {
+  vpc_id = aws_vpc.VPC_Pragma.id
+
+  tags = merge(
+    var.info_tags, {
+      Name = "Gateway_Pragma"
+    }
+  )
+}
